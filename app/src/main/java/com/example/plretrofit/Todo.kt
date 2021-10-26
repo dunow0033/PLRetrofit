278a2a5dbf1e1238c22aee13ca307f9fc06af4e8
Brian Donnoe <dunow33@hotmail.com>
2021-10-26 11:09:29 -0400

    first commit

---
+++ app/src/main/java/com/example/plretrofit/Todo.kt
@@ -0,0 +1,10 @@
+package com.example.plretrofit
+
+import com.google.gson.annotations.SerializedName
+
+data class Todo(
+    val completed: Boolean,
+    val id: Int,
+    val title: String,
+    val userId: Int
+)
